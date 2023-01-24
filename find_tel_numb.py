f2e71e1d7b69a66f475696885cf829736ae62b65
SelmarionX <selmarionx@gmail.com>
2023-01-24 10:41:55 +0300

    find_tel

---
+++ find_tel_numb.py
@@ -0,0 +1,17 @@
+import csv
+
+csvfile = 'base.csv'
+
+
+def find_data(field, name_string):
+    reader = {}
+    result_search = []
+    with open(csvfile, 'r', encoding="utf-8") as file:
+
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            if row[field] == name_string:
+                result_search += row.values()
+        if result_search == []:
+            print('данные не найдены')
+        return result_search
